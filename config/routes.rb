e2a2d5cd9e32998780c69f3bd9304018a06b66d0
timhutch79git config --global user.name tim.hutch.work@gmail.com <timhutchison@Tims-MacBook-Pro.local>
2018-12-23 17:25:33 +1000

    added profile page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 	root 'pages#home'
 	get 'about' => 'pages#about'
 	get 'contactus' => 'pages#contactus'
+  get 'profile' => 'pages#profile'
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
